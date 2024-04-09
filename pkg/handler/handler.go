20c98dfcce08fca0b3dc36db7d6b73512ea9bada
Manifoldz <56.zabolotnyi@gmail.com>
2024-04-09 14:23:50 +0500

    added handler

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -7,4 +7,19 @@ type Handler struct {
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
+
+	user_banner := router.Group("/user_banner")
+	{
+		user_banner.GET("/")
+	}
+
+	banner := router.Group("/banner")
+	{
+		banner.GET("/")
+		banner.POST("/")
+		banner.PATCH("/:id")
+		banner.DELETE("/:id")
+	}
+
+	return router
 }
