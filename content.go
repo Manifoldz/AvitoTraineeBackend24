ae654c6138f549576bc2dec36b70eaa8d032e28e
Manifoldz <56.zabolotnyi@gmail.com>
2024-04-10 13:30:40 +0500

    update DB schema

--- content.go
+++ content.go
@@ -1,7 +1,6 @@
 package banner
 
 type Content struct {
-	Id    int    `json:"-"`
 	Title string `json:"title"`
 	Text  string `json:"text"`
 	URL   string `json:"url"`
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,43 +1,30 @@
 CREATE TABLE banners
 (
     id SERIAL PRIMARY KEY,
-	feature_id int NOT NULL,
-	content_id int NOT NULL,
+	content JSONB NOT NULL,
 	is_active  boolean NOT NULL,
 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (feature_id) REFERENCES features (id) ON DELETE CASCADE,
-    FOREIGN KEY (content_id) REFERENCES contents (id) ON DELETE CASCADE,
+	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE features
 (
     id SERIAL PRIMARY KEY,
-	name_feature varchar(255) NOT NULL
+    name varchar(255) NOT NULL
 );
 
 CREATE TABLE tags
 (
     id SERIAL PRIMARY KEY,
-	name_tag varchar(255) NOT NULL
+    name varchar(255) NOT NULL
 );
-
-CREATE TABLE contents
-(
-    id SERIAL PRIMARY KEY,
-	title varchar(255) NOT NULL,
-	text_content  text NOT NULL,
-	url_content   varchar(4096) NOT NULL
-);
-
 
 CREATE TABLE BannerTags
 (
-	banner_id  int NOT NULL,
-	tag_id     int NOT NULL,
-    PRIMARY KEY (banner_id, tag_id),
-    FOREIGN KEY (banner_id) REFERENCES banners (id) ON DELETE CASCADE,
-    FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE
+    banner_id int NOT NULL REFERENCES banners (id) ON DELETE CASCADE,
+    feature_id int NOT NULL REFERENCES features (id) ON DELETE CASCADE,
+    tag_id int NOT NULL REFERENCES tags (id) ON DELETE CASCADE,
+    PRIMARY KEY (banner_id, feature_id, tag_id)
 );
 
 /* В PostgreSQL нет встроенного функционала ON UPDATE, поэтому для автоматического обновления поля updated_at при каждом изменении записи необходимо создать триггер:
